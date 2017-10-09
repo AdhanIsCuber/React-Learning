43bf99045905a7002f97bd94a1fd22737535f637
Wirya Ramadhan <AdhanIsCuber@gmail.com>
2017-10-09 15:19:10 +0700

    day 2 - state (chapter 6 of 30)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import MenuIcon from './MenuIcon';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchVisible: false
+        }
+    }
+
+    showSearch() {
+        this.setState({
+            searchVisible: !this.state.searchVisible
+        })
+    }
+
     render() {
+        let searchInputClasses = ["searchInput"];
+
+        if (this.state.searchVisible) {
+            searchInputClasses.push("active");
+        }
+
         return (
             <div className="header">
-                <div className="fa fa-bars menuIcon">
-                </div>
+                <MenuIcon />
 
                 <span className="title">
                     {this.props.title}
                 </span>
 
-                <input type="text" className="searchInput" placeholder="Search..."/>
-                <div className="fa fa-search searchIcon"></div>
+
+                <input 
+                    type="text" 
+                    className={searchInputClasses.join(' ')} 
+                    placeholder="Search..."/>
+                <div 
+                    onClick={this.showSearch.bind(this)}
+                    className="fa fa-search searchIcon"></div>
             </div>
         )
     }
---
+++ src/components/MenuIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MenuIcon = (props) => {
+  return (
+    <div className="menuIcon">
+			<div className="dashTop" />
+			<div className="dashBottom" />
+			<div className="circle" />
+		</div>
+  )
+  
+}
+
+export default MenuIcon;
